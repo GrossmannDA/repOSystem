c8c208ba87191d542accc8abb6c67690be5abd2a
sasha <o.baliev@yieldlab.de>
2018-02-04 22:07:30 +0100

    Lessons notes for 2018-02-01

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,12 +2,14 @@ package com.company;
 
 
 import com.company.presentation.controller.BusinessLogic;
+import com.company.service.BoardService;
+
 import java.io.IOException;
 
 public class Main {
 
   public static void main(String[] args) throws IOException, ClassNotFoundException {
-    BusinessLogic businessLogic = new BusinessLogic();
+    BusinessLogic businessLogic = new BusinessLogic(new BoardService());
     businessLogic.runApplication();
   }
 
